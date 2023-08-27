f6f540bb072fe566c2706fb251eeee40f85abf7d
bnb <deathproooof@gmail.com>
2023-08-27 20:11:31 +0900

    heroku対応

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_25_083432) do
-  create_table "posts", force: :cascade do |t|
-    t.string "name"
-    t.string "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "quotes", force: :cascade do |t|
+  create_table "quotes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
