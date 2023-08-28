00149d8b2ce55e5ac98ef6d5f3d115ce95fff303
bnb <deathproooof@gmail.com>
2023-08-28 11:07:30 +0900

    jsでリロードして無理やり空にして尚且つフォーカスする

--- app/javascript/toBlank.js
+++ app/javascript/toBlank.js
@@ -1,10 +1,12 @@
+const formArea = document.querySelector('.container_new form');
 const inputArea = document.querySelector(".container_new [type='text']");
 
-// 以下は実験用。ちなみにsubmitイベントで消すと、空白でsubmitされるのでバリデーションエラーに。
-const submitButton = document.querySelector(".container_new [type='submit']");
-const pageTitle = document.querySelector('h1');
-const formArea = document.querySelector('.container_new form');
-
-inputArea.addEventListener('focus',()=>{
-    inputArea.value = '';
+formArea.addEventListener('submit',()=>{
+    setTimeout( ()=> {
+        location.reload();
+    }, 1000);
 });
+
+window.onload = ()=> {
+    inputArea.focus();
+};
